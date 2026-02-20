ebc135f193bdd09473aaf0df592feb177f6f4af8
carlton.brown <carltonhbrn@gmail.com>
2026-02-19 20:57:06 -0600

    update monitor access

--- Services/Monitors/MicrosoftWindows/MonitorService.cs
+++ Services/Monitors/MicrosoftWindows/MonitorService.cs
@@ -9,19 +9,14 @@ namespace StartupScriptApp.Services.Monitors.MicrosoftWindows;
 
 public class WindowsMonitorManagement(Arguments arguments, ILogger logger) : IMonitorManager
 {
-    // Top left is 0,0 
+    // Top left is 0,0
     // Bottom right is Width, Height
-  
+
     private readonly bool _useDefaultMonitor = arguments.HasFlag("use-default-monitor");
+
     public List<MonitorInfo> GetAllMonitors()
     {
-        List<MonitorInfo> monitors;
-            
-        if (_useDefaultMonitor)
-            monitors = ConfigurationsDefaults.Monitors;
-        else
-            monitors = GetOsMonitors();
-        return monitors;
+        return _useDefaultMonitor ? ConfigurationsDefaults.Monitors : GetOsMonitors();
     }
 
     private List<MonitorInfo> GetOsMonitors()
@@ -31,15 +26,13 @@ public class WindowsMonitorManagement(Arguments arguments, ILogger logger) : IMo
 
         try
         {
-            MonitorInterop.EnumDisplayMonitors(IntPtr.Zero, IntPtr.Zero, MonitorInfoCallback,
-                GCHandle.ToIntPtr(gcHandle));
-            
-            list.Sort((a, b) =>
-            {
-                var topCompare = a.Bounds.Top.CompareTo(b.Bounds.Top);
-                return topCompare != 0 ? topCompare : a.Bounds.Left.CompareTo(b.Bounds.Left);
-            });
-            
+            MonitorInterop.EnumDisplayMonitors(
+                IntPtr.Zero,
+                IntPtr.Zero,
+                MonitorInfoCallback,
+                GCHandle.ToIntPtr(gcHandle)
+            );
+
             var index = 0;
             list.ForEach(monitor => monitor.Index = index++);
             logger.PrintMonitors("Monitors loaded from OS ", list);
@@ -49,17 +42,48 @@ public class WindowsMonitorManagement(Arguments arguments, ILogger logger) : IMo
         {
             gcHandle.Free();
         }
-      
     }
 
-    private bool MonitorInfoCallback(IntPtr hMonitor, IntPtr hdcMonitor, ref RECT lprcMonitor, IntPtr dwData)
+    /// <summary>
+    /// Depricated, Needs more testing but trust the order returned as the left to right, top to buttom list.
+    /// </summary>
+    /// <param name="monitors"></param>
+    /// <returns></returns>
+    private List<MonitorInfo> OrganizeMonitors(List<MonitorInfo> monitors)
+    {
+        var list = monitors;
+        
+        list.Sort(
+            (a, b) =>
+            {
+                var topCompare = a.Bounds.Top.CompareTo(b.Bounds.Top);
+                return topCompare != 0 ? topCompare : a.Bounds.Left.CompareTo(b.Bounds.Left);
+            }
+        );
+        
+        return list;
+    }
+
+    private bool MonitorInfoCallback(
+        IntPtr hMonitor,
+        IntPtr hdcMonitor,
+        ref RECT lprcMonitor,
+        IntPtr dwData
+    )
     {
         var monitors = (List<MonitorInfo>)GCHandle.FromIntPtr(dwData).Target!;
-        
+
         var mi = new OSMONITORINFO { cbSize = (uint)Marshal.SizeOf<OSMONITORINFO>() };
-    
+
         if (MonitorInterop.GetMonitorInfo(hMonitor, ref mi))
-            monitors.Add(new MonitorInfo { Bounds = mi.rcMonitor, WorkArea = mi.rcWork , IsPrimary = mi.dwFlags == SnapConstants.MONITORINFOF_PRIMARY});
+            monitors.Add(
+                new MonitorInfo
+                {
+                    Bounds = mi.rcMonitor,
+                    WorkArea = mi.rcWork,
+                    IsPrimary = mi.dwFlags == SnapConstants.MONITORINFOF_PRIMARY,
+                }
+            );
         else
         {
             logger.LogError();
